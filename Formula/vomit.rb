5149145ebc64a7af08c9e949ff66af81ac8fc9e4
shinji ikari <evapilotno17@gmail.com>
2025-04-23 02:20:33 +0530

    version update

--- Formula/vomit.rb
+++ Formula/vomit.rb
@@ -2,7 +2,7 @@ class Vomit < Formula
     desc "Recursive file content dumper"
     homepage "https://github.com/evapilotno17/vomit"
     url "https://github.com/evapilotno17/vomit/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "138a2b7eb8cf3a72c6269e3e7f3c78f2c9985e43f528ddd2774b845c88e1871a"
+    sha256 "70444d9d523d9e8a1fa2dec676510b6f2123855ff1d46d7c2f4acf2ec1a1b109"
     version "0.1.0"
   
     depends_on "python@3.12"
